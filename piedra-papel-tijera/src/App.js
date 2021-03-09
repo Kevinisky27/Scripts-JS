5c78cecbdb6b8a7ef7b6e6f2a26463a5e49296e2
Kevinisky27 <keare27@gmail.com>
2021-03-08 19:18:30 -0600

    Proyect Rock, Paper, Scissors

---
+++ piedra-papel-tijera/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Header from './header';
+import styled from 'styled-components';
+import Wrapper from './wrapper';
+
+const AppStyled =  styled.main`
+  @import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap');
+  
+  background-image: radial-gradient(circle at top, #1F3757 20%, #111f43 100%); 
+  min-height: 100vh;  
+  padding: 2em 0;
+
+  body{
+    font-family: 'Barlow Semi Condensed', sans-serif;
+  }
+  
+` 
+
+
+function App() {
+  return (
+    <AppStyled>
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    </AppStyled>
+  );
+}
+
+export default App;
